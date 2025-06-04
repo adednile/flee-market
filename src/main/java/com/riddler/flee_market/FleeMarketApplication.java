e72ef75a04a7e21f7fc1455137e9864fe2bf2915
JasonKuria <kuriajason32@gmail.com>
2025-06-04 23:01:49 +0300

    logger

--- src/main/java/com/riddler/flee_market/FleeMarketApplication.java
+++ src/main/java/com/riddler/flee_market/FleeMarketApplication.java
@@ -2,11 +2,15 @@ package com.riddler.flee_market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class FleeMarketApplication {
+    private static final Logger logger = LoggerFactory.getLogger(FleeMarketApplication.class);
 
     public static void main(String[] args) {
+        logger.info("Starting Flee Market Application...");
         SpringApplication.run(FleeMarketApplication.class, args);
     }
 
